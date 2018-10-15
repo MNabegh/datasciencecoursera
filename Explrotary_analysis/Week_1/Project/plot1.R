58a7a9ccb851734a493b593f9ceeabc8cbfa5d77
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2018-10-15 12:20:11 +0200

    add plots

---
+++ Explrotary_analysis/Week_1/Project/plot1.R
@@ -0,0 +1,47 @@
+#------------------------------------------------------------------------------------------------------------------
+# John Hopkins coursera data science specilization, Exploratory analysis course
+# project 1.
+#------------------------------------------------------------------------------------------------------------------
+
+setwd("~/DataScience/datasciencecoursera/Explrotary_analysis/Week_1/Project")
+library(data.table)
+library(dplyr)
+
+#------------------------------------------------------------------------------------------------------------------
+# 1. Download and unzip the file
+#------------------------------------------------------------------------------------------------------------------
+
+fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destFile = "Electric power consumption.zip"
+
+if (!file.exists(destFile))
+    download.file(fileUrl, destfile = destFile, mode = "wb")
+
+folderName = "household_power_consumption.txt"
+
+if(!file.exists(folderName))
+    unzip(destFile)
+
+rm(destFile, folderName, fileUrl)
+
+#------------------------------------------------------------------------------------------------------------------
+# 2. Reading data into R and Cleaning it
+#------------------------------------------------------------------------------------------------------------------
+data = data.table::fread("household_power_consumption.txt", sep = ';', na.strings = "?")
+data[, 'Date'] = as.Date(data[[1]], format = "%d/%m/%Y")
+data = filter(data, Date <= as.Date("2007-02-02") & Date >= as.Date("2007-02-01"))
+data[, 'Time'] = paste(data[[1]], data[[2]], sep = " ")
+data = data %>% rename(timestamp = Time) %>% select(-c(Date)) %>%
+    mutate(timestamp = as.POSIXct(timestamp),
+           day_of_the_week = weekdays.POSIXt(timestamp)) 
+
+
+#------------------------------------------------------------------------------------------------------------------
+# 3. Plotting
+#------------------------------------------------------------------------------------------------------------------
+
+hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)",
+     col = 'red', main = "Global Active Power")
+
+dev.copy2pdf(file = 'plot1.pdf')
+dev.off()
---
+++ Explrotary_analysis/Week_1/Project/plot3.R
@@ -0,0 +1,53 @@
+#------------------------------------------------------------------------------------------------------------------
+# John Hopkins coursera data science specilization, Exploratory analysis course
+# project 1.
+#------------------------------------------------------------------------------------------------------------------
+
+setwd("~/DataScience/datasciencecoursera/Explrotary_analysis/Week_1/Project")
+library(data.table)
+library(dplyr)
+
+#------------------------------------------------------------------------------------------------------------------
+# 1. Download and unzip the file
+#------------------------------------------------------------------------------------------------------------------
+
+fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destFile = "Electric power consumption.zip"
+
+if (!file.exists(destFile))
+    download.file(fileUrl, destfile = destFile, mode = "wb")
+
+folderName = "household_power_consumption.txt"
+
+if(!file.exists(folderName))
+    unzip(destFile)
+
+rm(destFile, folderName, fileUrl)
+
+#------------------------------------------------------------------------------------------------------------------
+# 2. Reading data into R and Cleaning it
+#------------------------------------------------------------------------------------------------------------------
+data = data.table::fread("household_power_consumption.txt", sep = ';', na.strings = "?")
+data[, 'Date'] = as.Date(data[[1]], format = "%d/%m/%Y")
+data = filter(data, Date <= as.Date("2007-02-02") & Date >= as.Date("2007-02-01"))
+data[, 'Time'] = paste(data[[1]], data[[2]], sep = " ")
+data = data %>% rename(timestamp = Time) %>% select(-Date) %>%
+    mutate(timestamp = as.POSIXct(timestamp),
+           day_of_the_week = weekdays.POSIXt(timestamp)) 
+
+
+#------------------------------------------------------------------------------------------------------------------
+# 3. Plotting
+#------------------------------------------------------------------------------------------------------------------
+
+plot(data$timestamp, data$Sub_metering_1, type = 'l',
+     xlab = "", ylab = "Energy Sub metering")
+lines(data$timestamp, data$Sub_metering_2, col = "red")
+lines(data$timestamp, data$Sub_metering_3, col = "blue")
+
+legend("topright", col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1, 1, 1))
+
+dev.copy2pdf(file = 'plot3.pdf')
+dev.off()
